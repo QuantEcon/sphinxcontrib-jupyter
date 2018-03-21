48c53ff827bc0a14d9b8b8a21f0b3a85f530c8a0
Matt McKay <mamckay@gmail.com>
2018-03-21 16:48:13 +1100

    update to minimal sphinx config for testscaes

--- tests/conf.py
+++ tests/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'sphinxcontrib-jupyter.minimal'
+project = 'sphinxcontrib-jupyter.testcases'
 copyright = '2018, QuantEcon Development Team'
 author = 'QuantEcon Development Team'
 
@@ -115,7 +115,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'sphinxcontrib-jupyterminimaldoc'
+htmlhelp_basename = 'sphinxcontrib-jupytertestcasedoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -142,7 +142,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sphinxcontrib-jupyterminimal.tex', 'sphinxcontrib-jupyter.minimal Documentation',
+    (master_doc, 'sphinxcontrib-jupytertestcases.tex', 'sphinxcontrib-jupyter.testcases Documentation',
      'QuantEcon Development Team', 'manual'),
 ]
 
@@ -152,7 +152,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sphinxcontrib-jupyterminimal', 'sphinxcontrib-jupyter.minimal Documentation',
+    (master_doc, 'sphinxcontrib-jupytertestcases', 'sphinxcontrib-jupyter.testcases Documentation',
      [author], 1)
 ]
 
@@ -163,8 +163,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sphinxcontrib-jupyterminimal', 'sphinxcontrib-jupyter.minimal Documentation',
-     author, 'sphinxcontrib-jupyterminimal', 'One line description of project.',
+    (master_doc, 'sphinxcontrib-jupytertestcases', 'sphinxcontrib-jupyter.testcases Documentation',
+     author, 'sphinxcontrib-jupytertestcases', 'One line description of project.',
      'Miscellaneous'),
 ]
 
@@ -205,7 +205,6 @@ jupyter_kernels = {
 # Configure Jupyter headers
 jupyter_headers = {
     "python3": [
-        # nbformat.v4.new_code_cell("%autosave 0")      #@mmcky please make this an option
     ],
     "julia": [
     ],
