4bad3c9f193158fb48be003b180de6187d7c04b8
Matt McKay <mamckay@gmail.com>
2018-03-21 16:29:27 +1100

    add simple script for comparing all ipynb files generated in _build/jupyter with a reference set of ipynb files

--- tests/check_diffs.py
+++ tests/check_diffs.py
@@ -3,16 +3,31 @@ Check diffs for each test file
 """
 
 import nbformat
+import glob
+import os
 from nbdime.diffing.notebooks import diff_notebooks
 
-fl1 = nbformat.read(
-    "_build/jupyter/simple_notebook.ipynb", 4)
-fl2 = nbformat.read(
-    "ipynb/simple_notebook.ipynb", 4)
+NB_VERSION = 4
+GENERATED_IPYNB_FILES = glob.glob("_build/jupyter/*.ipynb")
+REFERENCE_IPYNB_FILES = [os.path.basename(
+    fl) for fl in glob.glob("ipynb/*.ipynb")]
+SKIP = ["index.ipynb"]
 
-results = diff_notebooks(fl1, fl2)
+for fl in GENERATED_IPYNB_FILES:
+    flname = fl.split("/")[-1]
+    if flname in SKIP:
+        continue
+    print("Testing {} ...".format(fl))
+    if flname not in REFERENCE_IPYNB_FILES:
+        print("[FAIL] Notebook {} has no matching test case in ipynb/".format(flname))
+        exit(1)
+    nb1 = nbformat.read(fl, NB_VERSION)
+    nb2 = nbformat.read(os.path.join("ipynb", flname), NB_VERSION)
+    diff = diff_notebooks(nb1, nb2)
+    if len(diff) != 0:
+        print("[FAIL] {} and {} are different:".format(
+            fl, os.path.join("ipynb", flname)))
+        print(diff)
+        exit(1)
 
-if len(results) == 0:
-    exit(0)
-else:
-    exit(1)
+exit(0)
