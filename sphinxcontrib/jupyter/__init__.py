2479bbc278350ce2fb0a88c2ebd1390ea238847c
Matt McKay <mamckay@gmail.com>
2018-04-20 15:05:06 +1000

    change default to empty string

--- sphinxcontrib/jupyter/__init__.py
+++ sphinxcontrib/jupyter/__init__.py
@@ -8,7 +8,7 @@ def setup(app):
     app.add_config_value("jupyter_conversion_mode", None, "jupyter")
     app.add_config_value("jupyter_write_metadata", True, "jupyter")
     app.add_config_value("jupyter_static_file_path", [], "jupyter")
-    app.add_config_value("jupyter_header_block", None, "jupyter")
+    app.add_config_value("jupyter_header_block", "", "jupyter")
     app.add_config_value("jupyter_options", None, "jupyter")
 
     return {
