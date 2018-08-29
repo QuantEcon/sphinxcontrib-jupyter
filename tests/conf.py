cfd1d3caf9a721cda9a3208fd1cdb3f5d8933d3c
arnavs <soodarnav01@gmail.com>
2018-08-29 13:37:09 -0700

    reset conf.py

--- tests/conf.py
+++ tests/conf.py
@@ -194,16 +194,16 @@ jupyter_kernels = {
     },
     "julia": {
         "kernelspec": {
-            "display_name": "Julia 1.0.0",
+            "display_name": "Julia 0.6.0",
             "language": "julia",
-            "name": "julia-1.0"
+            "name": "julia-0.6"
         },
         "file_extension": ".jl"
     }
 }
 
 # Default language for Jupyter notebooks
-jupyter_default_lang = "julia"
+jupyter_default_lang = "python3"
 
 # Prepend a Welcome Message to Each Notebook
 jupyter_welcome_block = "welcome.rst"
