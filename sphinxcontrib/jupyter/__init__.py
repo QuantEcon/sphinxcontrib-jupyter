e330273750bec63bfbdb2f7026688c878ab44389
arnavs <soodarnav01@gmail.com>
2018-08-29 17:43:50 -0700

    update default values, False => True

--- sphinxcontrib/jupyter/__init__.py
+++ sphinxcontrib/jupyter/__init__.py
@@ -10,8 +10,8 @@ def setup(app):
     app.add_config_value("jupyter_header_block", "", "jupyter")
     app.add_config_value("jupyter_options", None, "jupyter")
     app.add_config_value("jupyter_default_lang", "python3", "jupyter")
-    app.add_config_value("jupyter_drop_solutions", False, "jupyter")
-    app.add_config_value("jupyter_drop_tests", False, "jupyter")
+    app.add_config_value("jupyter_drop_solutions", True, "jupyter")
+    app.add_config_value("jupyter_drop_tests", True, "jupyter")
     
 
     return {
