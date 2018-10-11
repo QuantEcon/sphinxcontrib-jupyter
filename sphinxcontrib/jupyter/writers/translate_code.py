7b6d502ddf5069e663b6dbe220408b220fda9b88
mmcky <mmcky@users.noreply.github.com>
2018-10-11 15:22:45 +1100

    FEAT: Add global ignore to no-execute code-blocks (#129)

--- sphinxcontrib/jupyter/writers/translate_code.py
+++ sphinxcontrib/jupyter/writers/translate_code.py
@@ -39,6 +39,7 @@ class JupyterCodeTranslator(docutils.nodes.GenericNodeVisitor):
         self.jupyter_write_metadata = builder.config["jupyter_write_metadata"]
         self.jupyter_drop_solutions = builder.config["jupyter_drop_solutions"]
         self.jupyter_drop_tests = builder.config["jupyter_drop_tests"]
+        self.jupyter_ignore_no_execute = builder.config["jupyter_ignore_no_execute"]
         self.jupyter_lang_synonyms = builder.config["jupyter_lang_synonyms"]
         self.jupyter_target_html = builder.config["jupyter_target_html"]
 
@@ -166,7 +167,7 @@ class JupyterCodeTranslator(docutils.nodes.GenericNodeVisitor):
     #  code blocks
     # ================
     def visit_literal_block(self, node):
-        _parse_class = JupyterOutputCellGenerators.GetGeneratorFromClasses(node.attributes['classes'])
+        _parse_class = JupyterOutputCellGenerators.GetGeneratorFromClasses(self, node)
         self.output_cell_type = _parse_class["type"]
         self.solution = _parse_class["solution"]
         self.test = _parse_class["test"]
