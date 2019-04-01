97d558829f1282cb5fa9a435d3947e7afe649f46
AakashGC <aakashguptachoudhury@gmail.com>
2019-04-01 15:42:25 +1100

    issue with json dump ascii

--- sphinxcontrib/jupyter/writers/execute_nb.py
+++ sphinxcontrib/jupyter/writers/execute_nb.py
@@ -196,8 +196,12 @@ class ExecuteNotebookWriter():
         json_data['run_time'] = time.strftime("%d-%m-%Y %H:%M:%S")
 
         try:
-            with open(json_filename, "w", encoding="UTF-8") as json_file:
-                json.dump(json_data, json_file)
+            if (sys.version_info > (3, 0)):
+                with open(json_filename, "w") as json_file:
+                    json.dump(json_data, json_file)
+            else:
+                with open(json_filename, "w") as json_file:
+                    json.dump(unicode(json_data), json_file)
         except IOError:
             self.logger.warning("Unable to save lecture status JSON file. Does the {} directory exist?".format(JUPYTER_REPORTS))
 
@@ -209,8 +213,12 @@ class ExecuteNotebookWriter():
         json_filename = JUPYTER_REPORTS + fln
 
         try:
-            with open(json_filename, "w", encoding="UTF-8") as json_file:
-                json.dump(builderSelf.dask_log, json_file)
+            if (sys.version_info > (3, 0)):
+                with open(json_filename, "w") as json_file:
+                    json.dump(builderSelf.dask_log, json_file)
+            else:
+                with open(json_filename, "w") as json_file:
+                    json.dump(unicode(builderSelf.dask_log), json_file)
         except IOError:
             self.logger.warning("Unable to save dask reports JSON file. Does the {} directory exist?".format(JUPYTER_REPORTS))
 
