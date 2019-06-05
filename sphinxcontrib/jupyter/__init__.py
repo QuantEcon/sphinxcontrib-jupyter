8101509324865c5181122eebbec5001bf2533ed7
AakashGC <aakashguptachoudhury@gmail.com>
2019-06-05 14:12:39 +1000

    adding default 1 for threads in builder init (#186)

--- sphinxcontrib/jupyter/__init__.py
+++ sphinxcontrib/jupyter/__init__.py
@@ -51,8 +51,8 @@ def setup(app):
     app.add_config_value("jupyter_execute_notebooks", False, "jupyter")
     app.add_config_value("jupyter_make_site", False, "jupyter")
     app.add_config_value("jupyter_dependency_lists", {}, "jupyter")
-    app.add_config_value("jupyter_threads_per_worker", None, "jupyter")
-    app.add_config_value("jupyter_number_workers", None, "jupyter")
+    app.add_config_value("jupyter_threads_per_worker", 1, "jupyter")
+    app.add_config_value("jupyter_number_workers", 1, "jupyter")
 
     # Jupyter Directive
     app.add_node(jupyter_node, html=(_noop, _noop), latex=(_noop, _noop))
