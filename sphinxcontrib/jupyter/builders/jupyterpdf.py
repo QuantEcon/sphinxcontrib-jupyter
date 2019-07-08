187cbb4d5179dd6c8ba1d322dc8ca5b89310dbc3
Aakash Gupta <aakashgfude@gmail.com>
2019-07-08 18:44:47 +1000

    rewriting init function

--- sphinxcontrib/jupyter/builders/jupyterpdf.py
+++ sphinxcontrib/jupyter/builders/jupyterpdf.py
@@ -26,9 +26,38 @@ class JupyterpdfBuilder(Builder):
     logger = logging.getLogger(__name__)
 
     def init(self):
-        print("what do we have here?")
+        # Check default language is defined in the jupyter kernels
+        def_lng = self.config["jupyter_default_lang"]
+        if  def_lng not in self.config["jupyter_kernels"]:
+            self.logger.warning(
+                "Default language defined in conf.py ({}) is not "
+                "defined in the jupyter_kernels in conf.py. "
+                "Set default language to python3"
+                .format(def_lng))
+            self.config["jupyter_default_lang"] = "python3"
+        # If the user has overridden anything on the command line, set these things which have been overridden.
+        instructions = []
+        overrides = self.config['jupyter_options']
+        if overrides:
+            instructions = overrides.split(",")
 
-		# start a dask client to process the notebooks efficiently. 
+        for instruction in instructions:
+            if instruction:
+                if instruction == 'code_only':
+                    self.config["jupyter_conversion_mode"] = "code"
+                else:
+                    # Fail on unrecognised command.
+                    self.logger.warning("Unrecognise command line parameter " + instruction + ", ignoring.")
+
+        #threads per worker for dask distributed processing
+        if "jupyter_threads_per_worker" in self.config:
+            self.threads_per_worker = self.config["jupyter_threads_per_worker"]
+
+        #number of workers for dask distributed processing
+        if "jupyter_number_workers" in self.config:
+            self.n_workers = self.config["jupyter_number_workers"]
+
+        # start a dask client to process the notebooks efficiently. 
         # processes = False. This is sometimes preferable if you want to avoid inter-worker communication and your computations release the GIL. This is common when primarily using NumPy or Dask Array.
         self.client = Client(processes=False, threads_per_worker = self.threads_per_worker, n_workers = self.n_workers)
         self.dependency_lists = self.config["jupyter_dependency_lists"]
@@ -36,6 +65,7 @@ class JupyterpdfBuilder(Builder):
         self.delayed_notebooks = dict()
         self.futures = []
         self.delayed_futures = []
+        
     def get_outdated_docs(self):
         for docname in self.env.found_docs:
             if docname not in self.env.all_docs:
@@ -58,11 +88,9 @@ class JupyterpdfBuilder(Builder):
 		Return the target URI for a document name.
 		typ can be used to qualify the link characteristic for individual builders.
 		"""
-        print(docname, "docname---")
         return docname
 
     def write_doc(self, docname, doctree):
-        print("write doc")
         doctree = doctree.deepcopy()
         destination = docutils.io.StringOutput(encoding="utf-8")
 
