ebfc1612eed00a337e211624a36cd24b0ecc746f
Aakash Gupta <aakashgfude@gmail.com>
2019-09-27 15:56:01 +1000

    adding special chars

--- sphinxcontrib/jupyter/writers/translate_all.py
+++ sphinxcontrib/jupyter/writers/translate_all.py
@@ -528,7 +528,7 @@ class JupyterTranslator(JupyterCodeTranslator, object):
             if self.jupyter_target_pdf:
                 uri_text = uri_text.lower()
                 SPECIALCHARS = [r"!", r"@", r"#", r"$", r"%", r"^", r"&", r"*", r"(", r")", r"[", r"]", r"{", 
-                                r"}", r"|", r":", r";", r",", r"?", r"'", r"’"]
+                                r"}", r"|", r":", r";", r",", r"?", r"'", r"’", r"–"]
                 for CHAR in SPECIALCHARS:
                     uri_text = uri_text.replace(CHAR,"")
                 formatted_text = " \\ref{" + uri_text + "}" #Use Ref and Plain Text titles
