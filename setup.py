fca462cbb4ccfdc572400e5fb0c51ee356859476
Matt McKay <mamckay@gmail.com>
2019-10-10 12:34:30 +1100

    add improved Description for PyPI

--- setup.py
+++ setup.py
@@ -4,9 +4,36 @@ from setuptools import setup, find_packages
 
 VERSION = 'v0.5.0'
 
-long_desc = '''
-This package contains the Jupyter Sphinx extension.
-'''
+LONG_DESCRIPTION = """
+This package contains a `Sphinx <http://www.sphinx-doc.org/en/master/>`_ for compiling 
+RST to Jupyter notebooks. It contains two primary builders:
+
+1. jupyter
+2. jupyterpdf
+
+The default behaviour of the `jupyter` builder is to provide notebooks that are readable 
+with an emphasis on adding markdown into the notebooks. However, it can also be configured
+to generate websites (such as `python.quantecon.org <https://python.quantcon.org>`_) 
+and run coverage tests, which uses `ipynb` as an intermediate format. 
+
+The `jupyterpdf` builder is focused on building `pdf` files (via an intermediate ipynb format).
+
+This project is maintained and supported by `QuantEcon <http://quantecon.org/>`.
+
+
+Status
+------
+
+|status-docs| |status-travis|
+
+.. |status-docs| image:: https://readthedocs.org/projects/sphinxcontrib-jupyter/badge/?version=latest
+   :target: http://sphinxcontrib-jupyter.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
+
+.. |status-travis| image:: https://travis-ci.org/QuantEcon/sphinxcontrib-jupyter.svg?branch=master
+    :target: https://travis-ci.org/QuantEcon/sphinxcontrib-jupyter
+
+"""
 
 requires = ['Sphinx>=0.6']
 
@@ -19,7 +46,7 @@ setup(
     author='QuantEcon',
     author_email='admin@quantecon.org',
     description='Sphinx "Jupyter" extension: Convert your RST files into executable Jupyter notebooks.',
-    long_description=long_desc,
+    long_description=LONG_DESCRIPTION,
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
