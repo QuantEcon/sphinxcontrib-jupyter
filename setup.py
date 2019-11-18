2047d3785076336f17d8ef16f14767fb44612b80
Matt McKay <mamckay@gmail.com>
2019-11-18 16:32:54 +1100

    update version for 0.5.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = 'v0.5.1'
+VERSION = 'v0.5.2'
 
 LONG_DESCRIPTION = """
 This package contains a `Sphinx <http://www.sphinx-doc.org/en/master/>`_ extension 
