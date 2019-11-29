f77fd3e07c251855108caab1486e5e01142ebd5c
Matt McKay <mamckay@gmail.com>
2019-11-29 13:32:38 +1100

    add a simple Jupyter Notebook object

--- sphinxcontrib/jupyter/writers/notebook.py
+++ sphinxcontrib/jupyter/writers/notebook.py
@@ -1,8 +1,22 @@
 """
 An Object Representing a Jupyter Notebook
+
+Example
+-------
+
+from notebook import JupyterNotebook
+nb = JupyterNotebook()
+nb.add_code_cell("import numpy as np", metadata={'collapse' : True})
+nb.add_markdown_cell("Hi")
+nb.add_raw_cell("--Bye--")
+nb.add_kernelspec("python3")
+nb.write("test.ipynb")
+
 """
 
-import nbformat.v4 as nbformat
+
+import nbformat
+import nbformat.v4 as nbapi
 from jupyter_client.kernelspec import get_kernel_spec, find_kernel_specs
 
 class JupyterKernelNotFound(Exception):
@@ -11,28 +25,78 @@ class JupyterKernelNotFound(Exception):
 class JupyterNotebook:
 
     def __init__(self):
-        self.notebook = nbformat.new_notebook()
+        """
+        A simple object that represents a Jupyter notebook
+        """
+        self.notebook = nbapi.new_notebook()
 
-    def add_code_cell(self, source, metadata=None):
-        code_cell = nbformat.new_code_cell(source)
+    def add_code_cell(self, source, metadata=None, **kwargs):
+        """
+        Append a Code Cell to the Notebook
+
+        Parameters
+        ----------
+        source : str
+        metadata : dict, optional(default=None)
+                   Add metadata to the cell
+        """
+        code_cell = nbapi.new_code_cell(source, **kwargs)
         if metadata:
-            for k,v in metadata.items():
-                code_cell.metadata[k] = v
+            code_cell = self.add_metadata(code_cell, metadata)
         self.notebook["cells"].append(code_cell)
 
-    def add_markdown_cell(self, formatted_text):
-        markdown_cell = nbformat.new_markdown_cell(formatted_text)
+    def add_markdown_cell(self, formatted_text, metadata=None, **kwargs):
+        """
+        Append a Markdown Cell to the Notebook
+
+        Parameters
+        ----------
+        formatted_text : str
+        """
+        markdown_cell = nbapi.new_markdown_cell(formatted_text, **kwargs)
+        if metadata:
+            markdown_cell = self.add_metadata(markdown_cell, metadata)
         self.notebook["cells"].append(markdown_cell)
 
+    def add_raw_cell(self, source, metadata=None, **kwargs):
+        """        
+        Append a Raw Cell to the Notebook
+
+        Parameters
+        ----------
+        source : str
+        """
+        raw_cell = nbapi.new_raw_cell(source, **kwargs)
+        if metadata:
+            raw_cell = self.add_metadata(raw_cell, metadata)
+        self.notebook["cells"].append(raw_cell)
+
     def add_kernelspec(self, language):
+        """
+        https://jupyter-client.readthedocs.io/en/stable/api/kernelspec.html
+        """
         self.kernelspec = get_kernel_spec(language)
-        # if language not in self.available_kernelspecs.keys():
-        #     msg = "Requested kernel: {} is not among the available kernels\n{}".format(language, self.available_kernels)
-        #     raise JupyterKernelNotFound(msg)
-        kernel = {
-            "kernelspec": {
+        kernelspec = {
                 "display_name": self.kernelspec.display_name,
-                "language": language,
-                }
+                "language": self.kernelspec.language,
+                "name" : language
             }
-        self.notebook.metadata.kernelspec = kernel
+        self.notebook.metadata.kernelspec = kernelspec
+
+    def write(self, fl):
+        """
+        https://nbformat.readthedocs.io/en/latest/api.html#nbformat.write
+        """
+        nbformat.write(self.notebook, fl)
+
+    def read(self, fl):
+        """
+        https://nbformat.readthedocs.io/en/latest/api.html#reading-and-writing
+        """
+        self.notebook = nbformat.read(fl)
+
+    def add_metadata(self, cell, metadata):
+        """ Attach Metadata to a cell """   
+        for k,v in metadata.items():
+            cell.metadata[k] = v
+        return cell
