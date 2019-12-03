38bad0d429fe14e12572daed8b63eada602efda4
Aakash Gupta <aakashgfude@gmail.com>
2019-12-03 12:19:34 +1100

    a simple jupyter builder

--- sphinxcontrib/jupyter/builders/jupyter.py
+++ sphinxcontrib/jupyter/builders/jupyter.py
@@ -18,7 +18,69 @@ import time
 from ..writers.utils import copy_dependencies
 
 class JupyterBuilder(Builder):
-    pass
+    """
+    Builds Jupyter Notebook
+    """
+    name = "jupyter"
+    format = "ipynb"
+    out_suffix = ".ipynb"
+    allow_parallel = True
+
+    _writer_class = JupyterWriter
+    logger = logging.getLogger(__name__)
+
+    def init(self):
+        pass
+
+    def get_outdated_docs(self):
+        for docname in self.env.found_docs:
+            if docname not in self.env.all_docs:
+                yield docname
+                continue
+            targetname = self.env.doc2path(docname, self.outdir,
+                                           self.out_suffix)
+            try:
+                targetmtime = os.path.getmtime(targetname)
+            except OSError:
+                targetmtime = 0
+            try:
+                srcmtime = os.path.getmtime(self.env.doc2path(docname))
+                if srcmtime > targetmtime:
+                    yield docname
+            except EnvironmentError:
+                pass
+
+    def get_target_uri(self, docname, typ=None):
+        return docname
+
+    def prepare_writing(self, docnames):
+
+        self.config["jupyter_conversion_mode"] = "code"
+        self.writer = self._writer_class(self)
+
+        ## copies the dependencies to the notebook folder
+        #copy_dependencies(self)
+            
+    def write_doc(self, docname, doctree):
+        doctree = doctree.deepcopy()
+        destination = docutils.io.StringOutput(encoding="utf-8")
+        self.writer.write(doctree, destination)
+
+        # get a NotebookNode object from a string
+        nb = nbformat.reads(self.writer.output, as_version=4)
+
+        outfilename = os.path.join(self.outdir, os_path(docname) + self.out_suffix)
+        ensuredir(os.path.dirname(outfilename))
+
+        try:
+            with open(outfilename, "wt", encoding="UTF-8") as f:
+                nbformat.write(nb, f)
+        except (IOError, OSError) as err:
+            self.logger.warning("error writing file %s: %s" % (outfilename, err))
+    
+    def finish(self):
+        pass
+
 
 #-Old-#
 
@@ -26,7 +88,7 @@ class JupyterBuilderOld(Builder):
     """
     Builds Jupyter Notebook
     """
-    name = "jupyter"
+    name = "jupyterold"
     format = "ipynb"
     out_suffix = ".ipynb"
     allow_parallel = True
