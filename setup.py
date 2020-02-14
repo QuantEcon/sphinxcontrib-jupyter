c68fc6db16300c3d0864445fa748b1267b9cac0f
Matt McKay <mamckay@gmail.com>
2020-02-14 15:37:53 +1100

    update minor version number for link to release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = 'v0.5.3'
+VERSION = 'v0.5.4'
 
 LONG_DESCRIPTION = """
 This package contains a `Sphinx <http://www.sphinx-doc.org/en/master/>`_ extension 
